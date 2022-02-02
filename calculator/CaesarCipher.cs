20dda48b9de21425dc138d907af7ffb5490c47bc
trexa81 <trex81@yandex.ru>
2022-02-02 11:56:55 +0300

    CaesarCipher

---
+++ calculator/CaesarCipher.cs
@@ -0,0 +1,40 @@
+﻿namespace calculator;
+
+public class CaesarCipher
+{
+    //символы русской азбуки
+    const string alfabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+
+    private string CodeEncode(string text, int k)
+    {
+        //добавляем в алфавит маленькие буквы
+        var fullAlfabet = alfabet + alfabet.ToLower();
+        var letterQty = fullAlfabet.Length;
+        var retVal = "";
+        for (int i = 0; i < text.Length; i++)
+        {
+            var c = text[i];
+            var index = fullAlfabet.IndexOf(c);
+            if (index < 0)
+            {
+                //если символ не найден, то добавляем его в неизменном виде
+                retVal += c.ToString();
+            }
+            else
+            {
+                var codeIndex = (letterQty + index + k) % letterQty;
+                retVal += fullAlfabet[codeIndex];
+            }
+        }
+
+        return retVal;
+    }
+
+    //шифрование текста
+    public string Encrypt(string plainMessage, int key)
+        => CodeEncode(plainMessage, key);
+
+    //дешифрование текста
+    public string Decrypt(string encryptedMessage, int key)
+        => CodeEncode(encryptedMessage, -key);
+}
--- calculator/Program.cs
+++ calculator/Program.cs
@@ -1,1 +1,14 @@
 ﻿
+using System.Text;
+using calculator;
+
+
+var cipher = new CaesarCipher();
+Console.Write("Введите текст: ");
+var message = Console.ReadLine();
+Console.Write("Введите ключ: ");
+var secretKey = Convert.ToInt32(Console.ReadLine());
+var encryptedText = cipher.Encrypt(message, secretKey);
+Console.WriteLine("Зашифрованное сообщение: {0}", encryptedText);
+Console.WriteLine("Расшифрованное сообщение: {0}", cipher.Decrypt(encryptedText, secretKey));
+Console.ReadLine();
