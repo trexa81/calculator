c2f2729463a371635fc0da5a7e43b034399ab483
trexa81 <trex81@yandex.ru>
2022-02-03 18:34:50 +0300

    почистил

--- calculator/CaesarCipher.cs
+++ calculator/CaesarCipher.cs
@@ -3,18 +3,14 @@
 public class CaesarCipher
 {
     /// <summary>
-    /// символы русской азбуки
+    /// алфавмт
     /// </summary>
-    const string alfabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+    const string fullAlfabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+    int letterQty = fullAlfabet.Length;     // Length:  Возвращает количество символов в текущей системе
 
-    private string CodeEncode(string text, int k)
+    private string CodeEncode(string text)
     {
-        
-        // алфавмт верхнего инижнего регистра
-        var fullAlfabet = alfabet + alfabet.ToLower(); // ToLower: Возвращает копию этой строки, преобразованную в нижний регистр абвгдеёжзийклмнопрстуфхцчшщъыьэюя
-        Console.WriteLine(fullAlfabet);
-        var letterQty = fullAlfabet.Length;     // Length:  Возвращает количество символов в текущей системе
-        var retVal = "";
+        string retVal = "";
         for (int i = 0; i < text.Length; i++)
         {
             var c = text[i];   // преобразует текст в массив чар
@@ -35,12 +31,12 @@ public class CaesarCipher
     }
 
     //шифрование текста
-    public string Encrypt(string plainMessage, int key)
-        => CodeEncode(plainMessage, key);
+    public string Encrypt(string plainMessage)
+        => CodeEncode(plainMessage);
 
     //дешифрование текста IСoder
-    public string Decrypt(string encryptedMessage, int key)
-        => CodeEncode(encryptedMessage, key);
+    public string Decrypt(string encryptedMessage)
+        => CodeEncode(encryptedMessage);
 }
 
 // Определить интерфейс IСoder, который полагает методы поддержки шифрования строк.
--- calculator/Program.cs
+++ calculator/Program.cs
@@ -4,11 +4,11 @@ using calculator;
 
 
 var cipher = new CaesarCipher();
-//Console.Write("Введите текст: ");
-var message = "АаБбяЯюЮ Введите текст:"; //Console.ReadLine();
-//Console.Write("Введите ключ: ");
-var secretKey = 1; //Convert.ToInt32(Console.ReadLine());
-var encryptedText = cipher.Encrypt(message, secretKey);
+
+var message = "АаБбяЯюЮ Введите текст:";
+
+
+var encryptedText = cipher.Encrypt(message);
 Console.WriteLine("Зашифрованное сообщение: {0}", encryptedText);
-Console.WriteLine("Расшифрованное сообщение: {0}", cipher.Decrypt(encryptedText, secretKey));
+Console.WriteLine("Расшифрованное сообщение: {0}", cipher.Decrypt(encryptedText));
 Console.ReadLine();
