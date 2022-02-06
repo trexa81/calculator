37924b26a167430f159736590dc939a9a850c5d5
trexa81 <trex81@yandex.ru>
2022-02-06 17:32:26 +0300

    финиш

--- calculator/CaesarCipher.cs
+++ calculator/CaesarCipher.cs
@@ -1,5 +1,4 @@
-﻿//namespace calculator;
-
+﻿
 public class CaesarCipher
 {
     /// <summary>
@@ -61,20 +60,11 @@ public class CaesarCipher
         => ACoder(encryptedMessage, -Key);
 
     //шифрование текста
-    public string Encrypt(string plainMessage)
+    public string BEncrypt(string plainMessage)
         => BCoder(plainMessage);
 
     //дешифрование текста IСoder
-    public string Decrypt(string encryptedMessage)
+    public string BDecrypt(string encryptedMessage)
         => BCoder(encryptedMessage);
 }
-
-// Определить интерфейс IСoder, который полагает методы поддержки шифрования строк.
-// В интерфейсе объявляются два метода Encode() и Decode(), используемые для шифрования и дешифрования строк.
-// Создать класс ACoder, реализующий интерфейс ICoder.
-// Класс шифрует строку посредством сдвига каждого символа на одну «алфавитную» позицию выше. (В результате такого сдвига буква А становится буквой Б).
-// Создать класс BCoder, реализующий интерфейс ICoder.
-// Класс шифрует строку, выполняя замену каждой буквы, стоящей в алфавите на i-й позиции, на букву того же регистра, расположенную в алфавите на i-й позиции с конца алфавита.
-// (Например, буква В заменяется на букву Э.
-// Написать программу, демонстрирующую функционирование классов).
 
--- calculator/Program.cs
+++ calculator/Program.cs
@@ -1,15 +1,13 @@
 ﻿
-//using System.Text;
-//using calculator;
 
-var key = -1;
+var key = 1;  // позиция сдвига
 var cipher = new CaesarCipher();
 var message = "АаБбяЯюЮ Введите текст:";
 var encryptedText = cipher.AEncrypt(message, key);
 Console.WriteLine("Зашифрованное сообщение A: {0}", encryptedText);
 Console.WriteLine("Расшифрованное сообщение: {0}", cipher.ADecrypt(encryptedText, key));
 
-encryptedText = cipher.Encrypt(message);
+encryptedText = cipher.BEncrypt(message);
 Console.WriteLine("Зашифрованное сообщение B: {0}", encryptedText);
-Console.WriteLine("Расшифрованное сообщение: {0}", cipher.Decrypt(encryptedText));
+Console.WriteLine("Расшифрованное сообщение: {0}", cipher.BDecrypt(encryptedText));
 Console.ReadLine();
