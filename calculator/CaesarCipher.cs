dce54ed8d8419b91b504c513c587b656cf26c552
trexa81 <trex81@yandex.ru>
2022-02-06 16:11:31 +0300

    ACoder + BCoder

--- calculator/CaesarCipher.cs
+++ calculator/CaesarCipher.cs
@@ -7,10 +7,11 @@ public class CaesarCipher
     /// </summary>
     const string fullAlfabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя";
     int letterQty = fullAlfabet.Length;     // Length:  Возвращает количество символов в текущей системе
+    
 
-    private string CodeEncode(string text)
+    private string ACoder(string text, int k = (-1))
     {
-        string retVal = "";
+        string retVal1 = "";
         for (int i = 0; i < text.Length; i++)
         {
             var c = text[i];   // преобразует текст в массив чар
@@ -18,25 +19,54 @@ public class CaesarCipher
             if (index < 0)
             {
                 //если символ не найден, то добавляем его в неизменном виде
-                retVal += c.ToString();
+                retVal1 += c.ToString();
+            }
+            else
+            {
+                var codeIndex = (letterQty + index + k) % letterQty;
+                retVal1 += fullAlfabet[codeIndex];
+            }
+        }
+
+        return retVal1;
+    }
+
+    private string BCoder(string text)
+    {
+        string retVal2 = "";
+        for (int i = 0; i < text.Length; i++)
+        {
+            var c = text[i];   // преобразует текст в массив чар
+            var index = fullAlfabet.IndexOf(c); // IndexOf: ипользует идех Юникода 
+            if (index < 0)
+            {
+                //если символ не найден, то добавляем его в неизменном виде
+                retVal2 += c.ToString();
             }
             else
             {
                 var codeIndex = (letterQty - index + 32) % letterQty;
-                retVal += fullAlfabet[codeIndex];
+                retVal2 += fullAlfabet[codeIndex];
             }
         }
 
-        return retVal;  // символ с новым индексом
+        return retVal2;  // символ с новым индексом
     }
+
+    public string AEncrypt(string plainMessage, int key)
+        => ACoder(plainMessage, key);
+
+    //дешифрование текста IСoder
+    public string ADecrypt(string encryptedMessage, int Key)
+        => ACoder(encryptedMessage, -Key);
 
     //шифрование текста
     public string Encrypt(string plainMessage)
-        => CodeEncode(plainMessage);
+        => BCoder(plainMessage);
 
     //дешифрование текста IСoder
     public string Decrypt(string encryptedMessage)
-        => CodeEncode(encryptedMessage);
+        => BCoder(encryptedMessage);
 }
 
 // Определить интерфейс IСoder, который полагает методы поддержки шифрования строк.
--- calculator/Program.cs
+++ calculator/Program.cs
@@ -2,13 +2,14 @@
 //using System.Text;
 //using calculator;
 
-
+var key = -1;
 var cipher = new CaesarCipher();
-
 var message = "АаБбяЯюЮ Введите текст:";
+var encryptedText = cipher.AEncrypt(message, key);
+Console.WriteLine("Зашифрованное сообщение A: {0}", encryptedText);
+Console.WriteLine("Расшифрованное сообщение: {0}", cipher.ADecrypt(encryptedText, key));
 
-
-var encryptedText = cipher.Encrypt(message);
-Console.WriteLine("Зашифрованное сообщение: {0}", encryptedText);
+encryptedText = cipher.Encrypt(message);
+Console.WriteLine("Зашифрованное сообщение B: {0}", encryptedText);
 Console.WriteLine("Расшифрованное сообщение: {0}", cipher.Decrypt(encryptedText));
 Console.ReadLine();
